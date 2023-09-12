e84c1695ab5e1ff75fc1e855966f1b220ee40217
Justin <justin@appint.cn>
2023-09-12 18:08:00 +0800

    upgrade

--- lib/src/widgets/code_wrapper.dart
+++ lib/src/widgets/code_wrapper.dart
@@ -38,7 +38,7 @@ class _PreWrapperState extends State<CodeWrapperWidget> {
                 onTap: () async {
                   if (hasCopied) return;
                   await Clipboard.setData(ClipboardData(text: widget.text));
-                  _switchWidget = Icon(Icons.check, size: 14, key: UniqueKey());
+                  _switchWidget = Icon(Icons.check, size: 14 , color: Colors.green, key: UniqueKey());
                   refresh();
                   Future.delayed(const Duration(seconds: 3), () {
                     hasCopied = false;
