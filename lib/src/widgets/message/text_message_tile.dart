b6b412341bd4db2fad238fa12552253487d2cc33
Justin <justin@appint.cn>
2024-08-09 13:45:05 +0800

    borderRadius

--- lib/src/widgets/message/text_message_tile.dart
+++ lib/src/widgets/message/text_message_tile.dart
@@ -204,9 +204,7 @@ class _TileTextMessageState extends State<TileTextMessage> {
                     if (enlargeEmojis)
                       SelectableText(widget.message.text, style: emojiTextStyle)
                     else
-                      Padding(
-                        padding: const EdgeInsets.only(left: 5),
-                        child: SelectionArea(
+                      SelectionArea(
                           child: TextMessageText(
                             bodyLinkTextStyle: bodyLinkTextStyle,
                             bodyTextStyle: bodyTextStyle,
@@ -215,7 +213,6 @@ class _TileTextMessageState extends State<TileTextMessage> {
                             options: widget.options,
                             text: widget.message.text,
                           ),
-                        ),
                       ),
                   if (widget.message.previewData?.image?.url != null)
                     _buildImagePreview(context),
